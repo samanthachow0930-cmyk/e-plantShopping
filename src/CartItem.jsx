c23954569e7c080bee49219ea7199b578baa792a
samanthachow0930-cmyk <samanthachow0930@gmail.com>
2025-08-21 12:59:27 -0400

    Modify 20250822

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => {
+        const cost = parseFloat(item.cost.substring(1));
+        return total + (cost * item.quantity);
+      }, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
-  const handleIncrement = (item) => {
-  };
+    const handleIncrement = (item) => {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
+    };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    }
+    else {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.substring(1));
+    const totalCost = cost * item.quantity;
+    return totalCost;
   };
 
   return (
